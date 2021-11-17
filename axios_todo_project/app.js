aed8bb0f76198ddd32f2f9d013d060b24f47bad5
Jonathan Barnum <jonbarnum07@gmail.com>
2021-11-17 13:29:40 -0700

    adding css, strikethrough and update with axios put

--- axios_todo_project/app.js
+++ axios_todo_project/app.js
@@ -1,42 +1,44 @@
 
 
-fullListPull = () => {
+const fullListPull = () => {
     axios.get("https://api.vschool.io/jonbarnum/todo")
     .then(response => {
-        for(let i = 0; i < response.data.length; i++){
-            const h1 = document.createElement('h1');
-            h1.textContent = `Title: ${response.data[i].title}`;
-            document.getElementsByClassName('todo')[0].appendChild(h1);
+        const data = response.data;
+        for(let i = 0; i < data.length; i++){
+            const div = document.createElement('div');
+            div.id = data[i]._id
+            document.getElementsByClassName('todo')[0].appendChild(div);
 
+            makingCheckBox(div, data[i]);
+            const h1 = document.createElement('h1');
+            h1.textContent = `Title: ${data[i].title}`;
+            if(data[i].completed){
+                h1.classList.add('todoHeader', 'done');
+            }else{
+                h1.classList.add('todoHeader');
+
+            }
+            div.appendChild(h1);
+            
             const liEl = document.createElement('li');
-            liEl.style.listStyleType = 'none';
-            liEl.textContent = `Description: ${response.data[i].description}`;
-            document.getElementsByClassName('todo')[0].appendChild(liEl);
+            liEl.classList.add('listItem');
+            liEl.textContent = `Description: ${data[i].description}`;
+            div.appendChild(liEl);
 
             const liEl2 = document.createElement('li');
-            liEl2.style.listStyleType = 'none';
-            liEl2.textContent = `Price: ${response.data[i].price}`;
-            document.getElementsByClassName('todo')[0].appendChild(liEl2)
-
-            const checkbox = document.createElement('input');
-            checkbox.type = 'checkbox';
-            checkbox.style.marginLeft = '0';
-            const label = document.createElement('label');
-            label.textContent = 'Have you completed this todo?';
-            document.getElementsByClassName('todo')[0].appendChild(checkbox);
-            document.getElementsByClassName('todo')[0].appendChild(label);
-
-            const liEl3 = document.createElement('li');
-            liEl3.style.listStyleType = 'none';
-            liEl3.textContent = `Completed: ${response.data[i].completed}`;
-            document.getElementsByClassName('todo')[0].appendChild(liEl3)
+            liEl2.classList.add('listItem');
+            liEl2.textContent = `Price: ${data[i].price}`;
+            div.appendChild(liEl2)
 
             let imageEl = document.createElement('img');
-            imageEl.src = response.data[i].imgUrl;
-            imageEl.width = '125';
-            imageEl.height = '100';
-            document.getElementsByClassName('todo')[0].appendChild(imageEl)
+            imageEl.src = data[i].imgUrl;
+            imageEl.classList.add('image');
+            div.appendChild(imageEl);
 
+            const liEl3 = document.createElement('li');
+            liEl3.classList.add('listItem')
+            liEl3.textContent = `Completed: ${data[i].completed}`;
+            div.appendChild(liEl3);
         }
     })
     .catch(error => console.log(error));
@@ -61,9 +63,43 @@ todoFormAdd.addEventListener('submit', function(event){
     todoFormAdd.imgUrl.value = '';
 
     axios.post("https://api.vschool.io/jonbarnum/todo", newTodo)
-        .then(response => fullListPull())
+        .then(response => fullListPull(response))
         .catch(error => console.log(error))
 })
+
+
+const strikeThrough = (event) => {
+    let checkbox = event.currentTarget;
+    let completed = checkbox.parentElement.children[5]
+    axios.put(`https://api.vschool.io/jonbarnum/todo/${checkbox.parentElement.id}`, {"completed": checkbox.checked})
+    .then(response => {
+        response.data.completed;
+        if(checkbox.checked){
+            checkbox.parentElement.children[1].classList.add('done');
+            completed.textContent = 'completed: true';
+        }else{
+            checkbox.parentElement.children[1].classList.remove('done');
+            completed.textContent = 'completed: false';
+        }
+    })
+    .catch(error => console.log(error))
+}
+
+const makingCheckBox = (div, todo) => {
+    const check = document.createElement('input');
+    check.type = 'checkbox';
+    check.checked = todo.completed;
+    check.classList.add('checkbox');
+    check.addEventListener('click', strikeThrough)
+    div.appendChild(check);
+}
+
+
+
+
+
+
+
 
 clearList = () => {
     const el = document.getElementsByClassName('todo')[0];
@@ -71,6 +107,8 @@ clearList = () => {
         el.remove(el.firstChild);
     }
 }
+
+
 
 fullListPull();
 
